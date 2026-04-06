48ab59468a32d620904f986cc57003047af3517f
unaimeds <versioncontrol.undamaged050@passinbox.com>
2026-04-06 18:33:44 +0200

    basic setup

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [preact(), tailwindcss(), tsconfigPaths()],
+});
