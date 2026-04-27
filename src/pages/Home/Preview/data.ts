1792f64298e46efe17722f2d7d04ccf56b1781c2
unaimeds <versioncontrol.undamaged050@passinbox.com>
2026-04-27 04:56:35 +0200

    use relative rice preview thumbnail image urls

--- src/pages/Home/Preview/data.ts
+++ src/pages/Home/Preview/data.ts
@@ -1,12 +1,10 @@
 import { RiceDto } from "@/types/dto";
 
-const pub = (file: string) => `http://localhost:5173/${file}`;
-
 export const DevRices: RiceDto[] = [
     {
         title: "Dracula eww Dashboard",
         price: 15.0,
-        thumbnailUrl: pub("thumb1.png"),
+        thumbnailUrl: "thumb1.png",
         tags: ["bspwm", "dracula", "eww"],
         downloads: 872,
         stars: 412,
@@ -14,21 +12,21 @@ export const DevRices: RiceDto[] = [
     {
         title: "Rosé Pine Moon",
         price: 10.0,
-        thumbnailUrl: pub("thumb2.png"),
+        thumbnailUrl: "thumb2.png",
         tags: ["bspwm", "rosé pine", "eww"],
         downloads: 2832,
         stars: 241,
     },
     {
         title: "Gruvbox Dark i3",
-        thumbnailUrl: pub("thumb3.png"),
+        thumbnailUrl: "thumb3.png",
         tags: ["i3", "gruvbox", "dark"],
         downloads: 8231,
         stars: 23,
     },
     {
         title: "Tokyo Night AwesomeWM",
-        thumbnailUrl: pub("thumb1.png"),
+        thumbnailUrl: "thumb1.png",
         tags: ["awesomewm", "tokyo night"],
         downloads: 83512,
         stars: 122,
@@ -36,7 +34,7 @@ export const DevRices: RiceDto[] = [
     {
         title: "system24 SwayWM",
         price: 12.34,
-        thumbnailUrl: pub("thumb2.png"),
+        thumbnailUrl: "thumb2.png",
         tags: ["sway", "dark"],
         downloads: 842,
         stars: 15,
