c211676e9753efdc7b50edf4ec49840aca9d202e
unaimeds <versioncontrol.undamaged050@passinbox.com>
2026-05-02 04:39:38 +0200

    not found page design

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -1,8 +1,18 @@
 export default function NotFound() {
     return (
-        <section>
-            <h1>404: Not Found</h1>
-            <p>It's gone :(</p>
-        </section>
+        <div className="flex flex-1 flex-col items-center justify-center text-center">
+            <h1 className="font-syne mb-2 text-2xl font-extrabold sm:text-4xl">
+                Not Found
+            </h1>
+            <p className="text-parchment/44 mb-6 text-base sm:text-2xl">
+                The page you're looking for could not be found
+            </p>
+            <a
+                href="/"
+                className="font-dm-mono bg-phosphor/20 border-phosphor border px-5 py-2 text-sm font-bold tracking-widest uppercase sm:text-lg"
+            >
+                Go back
+            </a>
+        </div>
     );
 }
